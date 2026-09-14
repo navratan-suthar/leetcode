3210bfdd5ea307a1855af9a344064b42bcd5ef5a
navratan-suthar <navratansuth1822@gmail.com>
2026-09-14 15:28:55 +0530

    Time: 4 ms (36.2%), Space: 43.7 MB (7.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> list = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                list.push(s.charAt(i));
+            }
+            else{
+                if(list.isEmpty()){
+                    return false;
+                }
+                else if((list.peek()=='(' && s.charAt(i)==')') ||
+                    (list.peek()=='{' && s.charAt(i)=='}') ||
+                    (list.peek()=='[' && s.charAt(i)==']')){
+                    list.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return list.isEmpty();
+    }
+}
